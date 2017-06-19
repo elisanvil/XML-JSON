df07d46aeb74f85aea127378e4b635144bc34973
Eliza Sanchez <iza_707@hotmail.com>
2017-06-19 06:20:16 -0500

    JSON

---
+++ 2_xmlHTTPRequest+JSON/app.js
@@ -0,0 +1,63 @@
+var http_request = false;
+
+function makeRequest(url) {
+
+
+    http_request = false;
+
+    if (window.XMLHttpRequest) { // Mozilla, Safari,...
+        http_request = new XMLHttpRequest();
+        if (http_request.overrideMimeType) {
+            http_request.overrideMimeType('text/plain');
+            // Ver nota sobre esta linea al final
+        }
+    } else if (window.ActiveXObject) { // IE
+        try {
+            http_request = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                http_request = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {}
+        }
+    }
+
+    if (!http_request) {
+        alert('Falla :( No es posible crear una instancia XMLHTTP');
+        return false;
+    }
+    http_request.onreadystatechange = alertContents;
+    http_request.open('GET', url, true);
+    http_request.send(null);
+
+}
+
+function alertContents() {
+    if (http_request.readyState == 4) {
+        if (http_request.status == 200) {
+            /*Aquí deben procesar el JSON y mostrar la respuesta en el HTML*/
+            
+            // Devuelve un JSON
+            var jsonDataRetrived = http_request.responseText;
+            // Parseamos
+            var canciones = JSON.parse(jsonDataRetrived);
+            // Recorremos el arreglo
+            var str = "";
+            for (var i = 0; i < canciones.length; i++) {
+                // Accedemos al atributo con .atributo
+                str = str + '<li>' + canciones[i].titulo + '</li>';
+            }
+            // Añadimos al DOM
+            document.getElementById("lista-canciones").innerHTML = str;
+
+        } else {
+            alert('Hubo problemas con la petición.');
+        }
+    }
+}
+
+window.onload = function() {
+    var link = document.getElementById('requerimiento');
+    link.onclick = function() {
+        makeRequest('datos.json');
+    }
+}
